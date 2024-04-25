52d3a36a3d77904cf9d803b5cb1325185afd9f9f
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-04-25 15:18:23 +0700

    Time: 0 ms (100.00%), Space: 7 MB (97.22%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
+private:
+    int dp[38];
 public:
     int tribonacci(int n) {
-        if (n < 1) {
+        if (n == 0) {
             return 0;
         }
-        if (n < 3) {
+        if (n == 1 || n == 2) {
             return 1;
         }
         
-        int dp[] = {0, 1, 1};
-        for (int i = 3; i <= n; ++i)
-            dp[i % 3] = dp[0] + dp[1] + dp[2];
-        return dp[n % 3];
+        if (dp[n]) {
+            return dp[n];
+        }
+    
+        
+        return dp[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 };
