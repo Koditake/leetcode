0e9be84c017a7a3e06404a9c22a54a00ca6682e0
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-01 10:14:21 +0700

    Time: 0 ms (100.00%), Space: 7.5 MB (65.62%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    auto reverseWord(std::string word) -> std::string {
+    auto reverseWord(std::string &word, int right) -> void {
         // in-place word reverse
-        int left = 0, right = word.size() - 1;
+        int left = 0;
         while (left < right) {
             std::swap(word[left], word[right]);
             left++;
             right--;
         }
-        return word;
     }
     
     string reversePrefix(string word, char ch) {
-        std::string res = {};
         for (int i = 0; i < word.size(); ++i) {
             if (word[i] == ch) {
-                res = reverseWord(word.substr(0,i+1));
-                res += word.substr(i+1,word.size() - i);
+                reverseWord(word,i);
                 break;
             }
         }
         
-        return (res.empty())?word:res;
+        return word;
     }
 };
