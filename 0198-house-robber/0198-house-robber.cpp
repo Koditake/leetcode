84b28a5d01df516f7e7ba5e80882c3057f6520ff
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-02 16:42:59 +0700

    Time: 0 ms (100.00%), Space: 9.2 MB (58.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-
+        
         if (n < 3) {
             return (n == 1)? nums[0]:std::max(nums[0], nums[1]);
         }
         
-        int dp[3] = {0};
-        dp[0] = nums[0];
-        dp[1] = std::max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i % 3] = std::max(dp[(i - 1) % 3], dp[(i - 2) % 3] + nums[i]);
+        std::vector<int> loot(n + 1);
+        loot[0] = nums[0];
+        loot[1] = std::max(nums[0], nums[1]);
+        
+        for (int i = 2; i < n; ++i) {
+            loot[i] = std::max(loot[i - 1], loot[i - 2] + nums[i]);
         }
-
-        return dp[(n - 1) % 3];
+        
+        return loot[n - 1];
     }
 };
