e9ea4b869b1a9d15842d18e8d4f11d2547b15d72
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-02 16:44:21 +0700

    Time: 0 ms (100.00%), Space: 9.1 MB (81.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,14 +7,16 @@ public:
             return (n == 1)? nums[0]:std::max(nums[0], nums[1]);
         }
         
-        std::vector<int> loot(n + 1);
-        loot[0] = nums[0];
-        loot[1] = std::max(nums[0], nums[1]);
+        int loot0 = nums[0],
+            loot1 = std::max(nums[0], nums[1]),
+            lootn = 0;
         
         for (int i = 2; i < n; ++i) {
-            loot[i] = std::max(loot[i - 1], loot[i - 2] + nums[i]);
+            lootn = std::max(loot1, loot0 + nums[i]);
+            loot0 = loot1;
+            loot1 = lootn;
         }
         
-        return loot[n - 1];
+        return lootn;
     }
 };
