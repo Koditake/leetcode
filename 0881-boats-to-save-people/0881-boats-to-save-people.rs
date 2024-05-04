6537122c27c5e7b47375bf7a1ee0cfa73bc075b2
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-04 16:53:32 +0700

    Time: 18 ms (82.35%), Space: 2.5 MB (5.88%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.rs
+++ 0881-boats-to-save-people/0881-boats-to-save-people.rs
@@ -1,14 +1,16 @@
 impl Solution {
     pub fn num_rescue_boats(people: Vec<i32>, limit: i32) -> i32 {
-        let mut ppl = people.clone();
-        ppl.sort();
-        //let (mut l, mut r) = (0, ppl.len() - 1);
+        let mut people = people;
+        people.sort();
+        // let (mut l, mut r) = (0, people.len() - 1);
+        
         let mut l:i32 = 0;
         let mut r:i32 = people.len() as i32 - 1;
+        
         let mut res = 0;
         
         while (l <= r) {
-            let sum = ppl[l as usize] + ppl[r as usize];
+            let sum = people[l as usize] + people[r as usize];
             if sum <= limit {
                 l += 1;
                 r -= 1;
