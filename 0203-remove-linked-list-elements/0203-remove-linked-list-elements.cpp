825f39bf2b81770496fd210b6569b46c2535a4ca
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-05 17:56:06 +0700

    Time: 20 ms (15.32%), Space: 18.5 MB (88.01%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr) 
+            return head;
+            
+        while (head != nullptr && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode    *curr = head;
+
+        while(curr != nullptr  && curr->next != nullptr){
+            if(curr->next->val == val){
+                curr->next = curr->next->next;
+				// After doing the above step, I am not updating "curr" because of these type of test cases:
+				// 1->2->3->6->6->6->5->null  val = 6
+            }
+            else
+                curr = curr->next;
+        }
+        return head;
+    }
+};
