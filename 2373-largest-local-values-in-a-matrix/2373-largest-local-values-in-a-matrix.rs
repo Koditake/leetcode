fc4d516eca91beb237612f864d6bb295d9e5fb54
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-12 14:44:25 +0700

    Time: 4 ms (90.91%), Space: 2.3 MB (9.09%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.rs
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.rs
@@ -3,7 +3,7 @@ use std::cmp::max;
 impl Solution {
     pub fn largest_local(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let n = grid.len();
-        let mut res = vec![vec![0 as i32;n - 2];n - 2];
+        let mut res = vec![vec![i32::MIN;n - 2];n - 2];
         
         for i in 0..n - 2 {
             for j in 0..n - 2 {
