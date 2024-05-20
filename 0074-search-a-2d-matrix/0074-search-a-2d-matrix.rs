ac249cbb41d3a53dbae71ef36eadf03ac7319cda
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-20 23:11:52 +0700

    Time: 1 ms (77.60%), Space: 2.1 MB (69.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -1,10 +1,21 @@
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        let what = matrix.binary_search_by(|num| num[0].cmp(&target));
-        match what {
-            Ok(_) => true,
-            Err(0) => false,
-            Err(index) => matrix[index -1].binary_search(&target).is_ok()
+        let rows: i32 = (matrix.len()) as i32;
+        let columns: i32 = (matrix[0].len()) as i32;
+
+        let mut start : i32 = 0;
+        let mut end : i32 = (rows * columns - 1) as i32;
+        while start <= end {
+            let mid = start + (end - start)/2;
+            let midValue = matrix[mid as usize / columns as usize][mid as usize % columns as usize];
+            if midValue == target{
+                return true;
+            } else if midValue < target{
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
+        return false;
     }
 }
