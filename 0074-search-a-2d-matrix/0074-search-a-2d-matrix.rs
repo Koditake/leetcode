7d6d92a8a544d93ac56120afb933febef1721850
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-20 23:12:17 +0700

    Time: 1 ms (77.60%), Space: 2.1 MB (69.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -7,15 +7,15 @@ impl Solution {
         let mut end : i32 = (rows * columns - 1) as i32;
         while start <= end {
             let mid = start + (end - start)/2;
-            let midValue = matrix[mid as usize / columns as usize][mid as usize % columns as usize];
-            if midValue == target{
+            let mid_value = matrix[mid as usize / columns as usize][mid as usize % columns as usize];
+            if mid_value == target{
                 return true;
-            } else if midValue < target{
+            } else if mid_value < target{
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return false;
+        false
     }
 }
