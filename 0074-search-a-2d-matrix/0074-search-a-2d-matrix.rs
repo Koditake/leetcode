c414e0ab7ad078dc22d15b62805dad7d2b88c2a4
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-05-21 16:19:10 +0700

    Time: 1 ms (77.32%), Space: 2.2 MB (14.95%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -1,19 +1,52 @@
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        let rows = matrix.len();
-        let cols = matrix[0].len();
-        let mut r = 0 as i32;
-        let mut c = cols as i32 - 1;
-        while r < rows as i32 && c as i32 >= 0 {
-            let curr = matrix[r as usize][c as usize];
-            if curr == target {
+        // let (m, n) = (matrix.len(), matrix[0].len());
+        let m = matrix.len();
+        let n = matrix[0].len();
+        
+        if target < matrix[0][0] || matrix[m - 1][n - 1] < target {
+            // target overmatched matrix
+            return false;
+        }
+    
+        let (mut h, mut k) = (0, m - 1);
+        while h <= k {
+            let mid = h + (k - h)/2;
+            if target == matrix[mid][0] || target == matrix[mid][n - 1] {
                 return true;
-            } else if curr < target {
-                r += 1;
+            } else if target < matrix[mid][0] {
+                k = mid - 1;
+            } else if target > matrix[mid][n - 1] {
+                h = mid + 1;
             } else {
-                c -= 1;
+                //row found, exit 1st binary search
+                h = mid;
+                break;
+            }
+        }    
+        
+        /*
+        // Check if the row has only one element
+        if n == 1 {
+          return matrix[h][0] == target;
+        }
+        */
+        
+        // h is now live and contain the row of target
+        let (mut l, mut r) = (0, n - 1);
+        
+        while l < r {
+            let mid = (l + (r - l)/2).wrapping_add(/* usize */ 1);
+            let val = matrix[h][mid];
+            if target > val {
+                l = mid + 1;
+            } else if target < val {
+                r = mid - 1;
+            } else {
+                return true;
             }
         }
+        
         false
     }
 }
