a686d7d95e0e1e11dd882791d44e0923ce30cf3e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-01 07:08:12 +0700

    Time: 5 ms (27.27%), Space: 7.7 MB (78.98%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res = 0;
+        
+        for (int i = 1; i < s.size(); i++) {
+            res += std::abs(s[i] - s[i - 1]);
+        }
+        
+        return res;
+    }
+};
