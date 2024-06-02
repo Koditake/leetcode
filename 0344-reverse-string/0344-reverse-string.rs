73572664d5844ef006c58856b41f1cbf2ead7b30
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-03 03:06:32 +0700

    Time: 13 ms (44.32%), Space: 5.5 MB (43.18%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.rs
+++ 0344-reverse-string/0344-reverse-string.rs
@@ -2,14 +2,7 @@ use std::mem;
 
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        let (mut left, mut right) = (0, s.len() - 1);
-        let mut swap;
-        while left < right {
-            swap = s[left];
-            s[left] = s[right];
-            s[right] = swap;
-            left += 1;
-            right -= 1;
-        }    
+        // why did I not thought about this before?!
+        s.reverse()
     }
 }
