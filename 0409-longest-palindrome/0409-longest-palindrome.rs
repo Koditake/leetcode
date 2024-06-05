cfc1b0a328edf7fff7454b23a37f3ce2c927cff3
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-05 18:21:16 +0700

    Time: 1 ms (67.27%), Space: 2.1 MB (52.73%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.rs
+++ 0409-longest-palindrome/0409-longest-palindrome.rs
@@ -1,22 +1,26 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn longest_palindrome(s: String) -> i32 {
-        let mut ch_map: HashMap<char, i32> = HashMap::new();
-        let mut res = 0;
-        let mut is_center = 0;
-
+        let mut lower_case = vec![0;26];
+        let mut upper_case = vec![0;26];
+        
         for c in s.chars() {
-            *ch_map.entry(c).or_insert(0) += 1;
-        }
-
-        for (_, &f) in ch_map.iter() {
-            res += f / 2;
-            if is_center == 0 && f % 2 == 1 {
-                is_center = 1;
+            if c.is_uppercase() {
+                upper_case[(c as u8 - 'A' as u8) as usize] += 1;
+            } else {
+                lower_case[(c as u8 - 'a' as u8) as usize] += 1;
             }
         }
-
-        res * 2 + is_center
+        
+        let mut res = 0; let mut is_center = 0;
+        
+        for i in 0..26 {
+            if is_center == 0 && (lower_case[i]%2 == 1 || upper_case[i]%2 == 1) {
+                is_center = 1;
+            }
+            res += (lower_case[i]/2)*2;
+            res += (upper_case[i]/2)*2;
+        }
+        
+        res + is_center as i32
     }
 }
