b5b7b5e98db0d4d357b9e159e11fdf7da6ebddd0
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-08 13:59:18 +0700

    Time: 1 ms (69.14%), Space: 2.2 MB (61.73%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
@@ -4,6 +4,19 @@ impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
         let (mut l, mut r) = (0, nums.len() - 1);
         
+        /*
+            PLAN:
+            -> Compare first and last
+                -> if First is NOT larger than last -> array is not tampered => return first
+                -> if First is larger than Last -> array is tampered => Apply solution
+
+            => SOLUTION using binary search:
+                -> find mid, compared with last
+                    -> mid is NOT smaller than last -> pivot is between mid and last
+                        => contract first to mid + 1, apply binary search
+                    -> mid is smaller than last -> pivot is between first and mid
+                        => contract last to mid, apply binary search.
+        */
         
         // array not rotated
         if nums[l] <= nums[r] {
@@ -16,7 +29,6 @@ impl Solution {
                 Ordering::Less => r = m,
                 _ => l = m + 1,
             }
-
         }
         
         return nums[l];
