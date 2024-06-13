2715619d4cff7ca08ccd4c8ea0ce8920da386e66
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-13 21:59:40 +0700

    Time: 30 ms (20.66%), Space: 21.2 MB (69.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,28 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode *head) {
-        ListNode *cur = head,
-                *pre = nullptr,
-                *tmp;
+    auto reverseList(ListNode *head) ->ListNode* {
+        if (!head) {
+            return nullptr;
+        }
+        
+        ListNode *pre = nullptr,
+                *cur = head,
+                *temp;
+        
         while (cur) {
-            tmp = cur->next;
+            temp = cur->next;
             cur->next = pre;
             pre = cur;
-            cur = tmp;
+            cur = temp;
         }
         
         return pre;
     }
     
-    void reorderList(ListNode *head) {
-    // Check for edge cases
+    void reorderList(ListNode* head) {
+        // Check for edge cases
         if(head->next == NULL || head->next->next == NULL)
             return;
 
         // Step 1 - Find middle of the LL with slow-fast pointer approach
-        ListNode *slow = head, *fast = head, *slow_prev = NULL;
-        while(fast != NULL && fast->next != NULL){
+        ListNode *slow = head, 
+                *fast = head, 
+                *slow_prev = nullptr;
+        while(fast != nullptr && fast->next != nullptr){
             slow_prev = slow;
             slow = slow->next;
             fast = fast->next->next;
@@ -39,19 +46,21 @@ public:
 
         // Step 2 - Split the LL into 2 parts from the middle 
         // and reverse the second part
-        ListNode *h1 = head, *h2;
-        if(fast == NULL){
+        ListNode *h1 = head, 
+                *h2;
+        if(fast == nullptr){
             h2 = reverseList(slow);
-            slow_prev->next = NULL;
+            slow_prev->next = nullptr;
         }
         else{
             h2 = reverseList(slow->next);
-            slow->next = NULL;
+            slow->next = nullptr;
         }
         
         // Traverse both the LL while linking heads of both LL
-        ListNode *next1 = h1->next, *next2 = h2->next;
-        while(h1 != NULL && h2 != NULL){
+        ListNode *next1 = h1->next, 
+                *next2 = h2->next;
+        while(h1 != nullptr && h2 != nullptr){
             next1 = h1->next;
             next2 = h2->next;
 
