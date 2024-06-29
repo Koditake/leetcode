7e775d0f36f59c658cf99ccab82fe673f46f6237
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-30 00:58:00 +0700

    Time: 43 ms (11.89%), Space: 21.5 MB (31.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    auto kokoEatBanan(std::vector<int> &piles, int speed) -> long {
-        long res = 0;
+    auto bananEatTime(int speed, vector<int> &piles) -> long long {
+        long long res = 0;
         for (auto &pile:piles) {
             res += ceil((double)pile/speed);
         }
@@ -10,23 +10,21 @@ public:
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        
-        int l = 1,
+        long long l = 1,
             r = *std::max_element(piles.begin(),piles.end());
-        int res = r;
+        long long res = r;
         while (l <= r) {
-            int m = l + (r - l)/2; //banan per hour
-            long v = kokoEatBanan(piles, m); //time to eat wrt to speed
+            long long m = l + (r - l)/2,
+                t = bananEatTime(m, piles);
             
-                if (h >= v) {
-                    res = std::min(res,m);
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }  
+            if (t > h) {
+                l = m + 1;
+            } else /*if (t <= h)*/ {
+                r = m - 1;
+                res = std::min(res,m);
+            }
         }
         
-        return res;
+        return (int)res;
     }
 };
