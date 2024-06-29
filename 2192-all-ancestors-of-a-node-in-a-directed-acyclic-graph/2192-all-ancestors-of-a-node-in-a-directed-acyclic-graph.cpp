bfc301ac6fa3911c186f06817202ac016b69e945
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-06-29 23:00:38 +0700

    Time: 96 ms (92.43%), Space: 63 MB (96.71%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> res(n), al(n);
-        function<void(int, int)> dfs = [&](int i, int anc){
-            for (auto j : al[i])
-                if (res[j].empty() || res[j].back() != anc) {
-                    res[j].push_back(anc);
-                    dfs(j, anc);    
-                }
-        };
-        
-        for (auto &e: edges)
-            al[e[0]].push_back(e[1]);
-        for (int i = 0; i < n; ++i)
-            dfs(i, i);
+        std::vector<vector<int>> res(n), directChild(n);
+        for(auto &e:edges) 
+            directChild[e[0]].push_back(e[1]);
+        for(int i = 0; i < n; i++)
+            dfs(i, i, res, directChild);
         return res;
+    }
+    auto dfs(int x, int curr, vector<vector<int>>& res, vector<vector<int>>& directChild) -> void {
+        for (auto &ch:directChild[curr]) 
+            if(res[ch].size() == 0 || res[ch].back() != x) {
+                res[ch].push_back(x);
+                dfs(x, ch, res, directChild);
+            }
     }
 };
