2e8f4687c44a4f8647ef337ca2335acaec6ed18e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-11 16:32:55 +0700

    Time: 52 ms (83.82%), Space: 51.9 MB (7.49%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,18 @@
+function searchInsert(nums: number[], target: number): number {
+    let l:number = 0,
+        r:number = nums.length - 1
+    
+    while (l <= r) {
+        let m = Math.floor((l + r)/2)
+        
+        if (nums[m] > target) {
+            r = m - 1;
+        } else if (nums[m] < target) {
+            l = m + 1;
+        } else {
+            return m;
+        }
+    }
+    
+    return l;
+};
