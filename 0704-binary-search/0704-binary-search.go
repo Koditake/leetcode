42ae2d301ab8a2a83e4be0c1a843a98cc58c865b
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-11 16:03:04 +0700

    Time: 26 ms (58.45%), Space: 6.8 MB (77.63%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.go
@@ -0,0 +1,17 @@
+func search(nums []int, target int) int {
+    l := 0;
+    r := len(nums) - 1;
+    
+    for l <= r {
+        m := l + (r - l)/2;
+        
+        if nums[m] > target {
+            r = m - 1;
+        } else if nums[m] < target {
+            l = m + 1;
+        } else {
+            return m;
+        }
+    }
+    return -1;
+}
