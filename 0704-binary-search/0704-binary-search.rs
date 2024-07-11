b679d09ac516a4dda7fa64bc63ba94c6aee24fa7
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-11 16:14:33 +0700

    Time: 3 ms (76.28%), Space: 2.2 MB (93.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.rs
+++ 0704-binary-search/0704-binary-search.rs
@@ -2,17 +2,20 @@ use std::cmp::Ordering::{Equal, Less, Greater};
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let (mut l, mut r) = (0, nums.len());
-
-        while l < r {
-            let m = l + (r - l) / 2;
-            match target.cmp(&nums[m]) {
-                Equal => return m as i32,
-                Less => r = m,
-                Greater => l = m + 1,
+        let mut l = 0 as i32;
+        let mut r = nums.len() as i32 - 1;
+        
+        while l <= r {
+            let m = l + (r - l)/2;
+            let mid = nums[m as usize];
+            
+            match mid.cmp(&target) {
+                Less => l = m + 1,
+                Equal => return m,
+                Greater => r = m - 1
             }
         }
-
+        
         -1
     }
 }
