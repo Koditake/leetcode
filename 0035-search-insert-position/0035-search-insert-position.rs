96784d357b17e4bfe07e56e65c23ffd1efa41466
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-11 16:26:43 +0700

    Time: 1 ms (85.25%), Space: 2.2 MB (11.71%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.rs
@@ -0,0 +1,21 @@
+use std::cmp::Ordering::{Equal, Less, Greater};
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut l = 0 as i32;
+        let mut r = nums.len() as i32 - 1;
+        
+        while l <= r {
+            let m = l + (r - l) / 2;
+            let mid = nums[m as usize];
+            
+            match mid.cmp(&target) {
+                Less => l = m + 1,
+                Equal => return m,
+                Greater => r = m - 1
+            }
+        }
+        
+        l
+    }
+}
