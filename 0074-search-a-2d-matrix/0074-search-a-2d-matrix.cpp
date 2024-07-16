63f4f3893997ccfb398dd63fe60e69507467a24e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-16 21:55:32 +0700

    Time: 7 ms (22.00%), Space: 12.2 MB (5.90%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,18 +5,21 @@ public:
             col = matrix[0].size();
         
         // check if target overshoot matrix
-        if (target < matrix[0][0] || matrix[row - 1][col - 1] < target) {
+        if (target < matrix[0][0] || matrix[row - 1][col - 1] < target)
             return false;
-        }
+        
+        // check if target is at matrix edges
+        if (target == matrix.front().front() || target == matrix.back().back()) 
+            return true;
         
         int l = 0,
             r = row * col - 1;
         
-        while (l <= r) {
+        while (l < r) {
             int m = l + (r - l)/2,
                 v = matrix[m / col][m % col];
             if (target < v) {
-                r = m - 1;
+                r = m;
             } else if (target > v) {
                 l = m + 1;
             } else /* target == v */ { 
