eeb5fc1d1a5d1392b254cf7a165e94fe1c3d25a2
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-18 00:50:01 +0700

    Time: 0 ms (100.00%), Space: 8.3 MB (5.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int l = 1, 
+            r = x/2;
+        while (l <= r) {
+            int m = l + (r - l)/2,
+                mid = x/m;
+            if (mid == m) {
+                return m;
+            } 
+            if (mid < m) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return r;
+    }
+};
