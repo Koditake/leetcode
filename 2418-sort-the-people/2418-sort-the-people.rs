c8a82246967fc6f095e6179dd5b4f762f3920b37
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-22 17:56:22 +0700

    Time: 8 ms (35.71%), Space: 2.3 MB (57.14%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.rs
+++ 2418-sort-the-people/2418-sort-the-people.rs
@@ -2,8 +2,6 @@ use std::cmp::Reverse;
 
 impl Solution {
     pub fn sort_people(names: Vec<String>, heights: Vec<i32>) -> Vec<String> {
-        let n = names.len();
-        
         // Create a vector of tuples (height, name)
         let mut map: Vec<(i32, String)> = heights.iter().zip(names.iter()).map(|(&height, name)| (height, name.clone())).collect();
 
@@ -11,9 +9,6 @@ impl Solution {
         map.sort_by_key(|&(height, _)| Reverse(height));
         
         // Extract names into result vector
-        let res: Vec<String> = map.iter().map(|(_, name)| name.clone()).collect();
-
-        // Return the result vector
-        res
+        return map.iter().map(|(_, name)| name.clone()).collect();
     }
 }
