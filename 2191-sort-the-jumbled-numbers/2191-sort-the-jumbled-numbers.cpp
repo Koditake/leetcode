5fda3805ea205b18e2e613ddb0ef2acf78b14177
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-24 18:11:04 +0700

    Time: 125 ms (97.40%), Space: 87.6 MB (72.29%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-    inline auto switcharoo(vector<int>& mapping, int num, int res = 0, int pow10 = 1) -> int {
+    inline auto switcharoo(vector<int>& mapping, int num) -> int {
         if (num < 10)
             return mapping[num];
+        
+        int res = 0,
+            pow10 = 1;
         
         while (num > 0) {
             res += mapping[num%10] * pow10;
@@ -13,22 +16,20 @@ public:
     }
     
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        std::vector<pair<int, int>> storePairs;
+        std::vector<pair<int, int>> vec_p;
 
         for (int i = 0; i < nums.size(); ++i) {
-            // Store the mapped value.
-            // int mappedValue = switcharoo(mapping, nums[i]);
             // Push a pair consisting of mapped value and original value's index.
-            storePairs.push_back({switcharoo(mapping, nums[i]), i});
+            vec_p.push_back({switcharoo(mapping, nums[i]), i});
         }
 
         // Sort the array in non-decreasing order by the first value (default).
-        std::sort(storePairs.begin(), storePairs.end());
-        std::vector<int> answer;
+        std::sort(vec_p.begin(), vec_p.end());
+        std::vector<int> res;
         
-        for (auto &[p1,p2]:storePairs)
-            answer.push_back(nums[p2]);
+        for (auto &[p1,p2]:vec_p)
+            res.push_back(nums[p2]);
         
-        return answer;
+        return res;
     }
 };
