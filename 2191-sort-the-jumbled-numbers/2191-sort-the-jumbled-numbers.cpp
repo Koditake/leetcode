dc33979aee4b7c1bed0c7ad9177ebad18e01e86f
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-24 15:33:22 +0700

    Time: 542 ms (22.51%), Space: 88.8 MB (55.84%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    auto switcharoo(std::vector<int> &mapping, int num) -> int {
-        int res = 0,
-            pow10 = 1;
-
-        if (num == 0)
-            return mapping[num];
-        
-        while (num > 0) {
-            res += mapping[(num % 10)] * pow10;
-            pow10 *= 10;
-            num /= 10;
-        }
-        
-        return res;
-    }
-    
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        std::map<int, std::vector<int>> map;
-        
-        for (auto &n:nums) {
-            map[switcharoo(mapping, n)].push_back(n);
+        vector<pair<int, int>> storePairs;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Convert current value to string
+            string number = to_string(nums[i]);
+            string formed = "";
+            for (int j = 0; j < number.size(); ++j) {
+                formed = formed + (to_string(mapping[number[j] - '0']));
+            }
+            // Store the mapped value.
+            int mappedValue = stoi(formed);
+            // Push a pair consisting of mapped value and original value;s
+            // index.
+            storePairs.push_back({mappedValue, i});
         }
-        
-        std::vector<int> res;
-        for (auto &[map,vec]:map) {
-            res.insert(res.end(), vec.begin(), vec.end());
+
+        // Sort the array in non-decreasing order by the first value (default).
+        sort(storePairs.begin(), storePairs.end());
+        vector<int> answer;
+        for (auto pair : storePairs) {
+            answer.push_back(nums[pair.second]);
         }
-        
-        return res;
+        return answer;
     }
 };
