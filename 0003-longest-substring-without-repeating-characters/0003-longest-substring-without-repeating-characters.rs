3ef5cf50cb7690632e0b7522d6defda93b824efc
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-07-29 14:51:28 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (28.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,25 +1,18 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let n = s.len();
-        let char_arr:Vec<char> = s.chars().collect();
-        let mut charset = HashSet::new();
+        let u8_arr:Vec<char> = s.chars().collect();
+        let mut res = 0;
         let mut l = 0;
-        let mut res:i32 = 0;
-
-        for r in 0..n {
-          if !charset.contains(&char_arr[r]) { // Use contains to check for existence
-            res = res.max((r - l + 1) as i32);
-          } else {
-            while charset.contains(&char_arr[r]) {
-              charset.remove(&char_arr[l]);
-              l += 1;
+        let mut u8_cnt = vec![0;256];
+        for r in 0..u8_arr.len() {
+            u8_cnt[u8_arr[r] as usize] += 1;
+            while u8_cnt[u8_arr[r] as usize] > 1 {
+                u8_cnt[u8_arr[l] as usize] -= 1;
+                l += 1;
             }
-          }
-          charset.insert(&char_arr[r]);
+            res = res.max(r - l + 1);
         }
-
-        res
+        
+        res as i32
     }
 }
