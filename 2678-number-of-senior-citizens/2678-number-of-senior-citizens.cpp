1e81901434a524256854e6b37f45bc5d8a53ef16
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-01 11:20:48 +0700

    Time: 3 ms (91.87%), Space: 17.7 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,7 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int res = 0;
         for (auto &str:details) {
-            if (stoi(str.substr(11,2)) > 60)
+            if ((str[11] - '0')*10 + (str[12] - '0') > 60)
                 ++res;
         }
         
