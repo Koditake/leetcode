c4f2d266b173528738202572a163dbbd3a82a3e3
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-03 13:46:59 +0700

    Time: 2 ms (63.64%), Space: 2.2 MB (63.64%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.rs
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.rs
@@ -1,20 +1,11 @@
-use std::collections::HashMap;
-
+use std::cmp::Ordering::{Equal};
 impl Solution {
     pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
-        let mut imap = HashMap::new();
+        let mut vec1 = target.clone();
+        vec1.sort();
+        let mut vec2 = arr.clone();
+        vec2.sort();
         
-        for i in 0..arr.len() {
-            *imap.entry(target[i]).or_insert(0) += 1;
-            *imap.entry(arr[i]).or_insert(0) -= 1;
-        }
-        
-        for (num,freq) in imap {
-            if freq != 0 {
-                return false;
-            }
-        }
-        
-        true
+        return vec1 == vec2;
     }
 }
