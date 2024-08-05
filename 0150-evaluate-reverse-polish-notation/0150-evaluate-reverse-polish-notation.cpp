08b3f194f6d8b8872efa8b62a90a74072afee6b2
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-05 14:02:15 +0700

    Time: 7 ms (67.88%), Space: 15.7 MB (42.90%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,25 +3,19 @@ public:
     int evalRPN(vector<string>& tokens) {
         std::vector<int> store;
         
-        for (auto const &t:tokens) {
-            if (t == "+") {
-                int n2 = store.back(); store.pop_back();
-                int n1 = store.back(); store.pop_back();
-                store.push_back(n1 + n2);
-            } else if (t == "-") {
-                int n2 = store.back(); store.pop_back();
-                int n1 = store.back(); store.pop_back();
-                store.push_back(n1 - n2);
-            } else if (t == "*") {
-                int n2 = store.back(); store.pop_back();
-                int n1 = store.back(); store.pop_back();
-                store.push_back(n1 * n2);
-            } else if (t == "/") {
-                int n2 = store.back(); store.pop_back();
-                int n1 = store.back(); store.pop_back();
-                store.push_back(n1 / n2);
-            } else {
+        for (auto &t:tokens) {
+            if (t.size() > 1 || isdigit(t[0])) {
                 store.push_back(stoi(t));
+            } else {
+                int n2 = store.back(); store.pop_back();
+                int n1 = store.back(); store.pop_back();
+                switch (t[0]) {
+                    case '+': store.push_back(n1 + n2); break;
+                    case '-': store.push_back(n1 - n2); break;
+                    case '*': store.push_back(n1 * n2); break;
+                    case '/': store.push_back(n1 / n2); break;
+                    defalt: continue; break;
+                }
             }
         }
         
