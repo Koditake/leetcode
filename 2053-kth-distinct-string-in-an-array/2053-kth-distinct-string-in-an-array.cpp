7c8f3fd9c8b058210429275555f79ca7a3665434
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-05 13:43:20 +0700

    Time: 17 ms (51.86%), Space: 20.1 MB (5.97%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+    auto find_pos(vector<tuple<string,int>> &vtup, string &str, int pos = 0) -> int {
+        for (auto &[s,c]:vtup)
+            if (s == str) return pos;
+            else ++pos;
+        return -1;
+    }
+    
     string kthDistinct(vector<string>& arr, int k) {
-        std::unordered_map<string, int> mstr;
-        for (auto const &s:arr) ++mstr[s];
+        std::unordered_set<string> uset;
+        std::vector<std::tuple<string,int>> vtup;
         
-        for (auto const &s:arr) {
-            if (mstr[s] == 1) {
+        for (auto &s:arr) {
+            if (!uset.contains(s)) {
+                uset.insert(s);
+                vtup.push_back({s,1});
+            } else {
+                std::get<1>(vtup[find_pos(vtup,s)]) += 1;
+            }
+        }
+        
+        for (auto const &[s,c]:vtup) {
+            if (c == 1) {
                 if (k == 1) return s;
                 else --k;
             }
