26e03ee10f841c50b18afcff52b274dd46689aa8
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-06 17:39:01 +0700

    Time: 4 ms (43.30%), Space: 14.7 MB (15.69%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,36 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (!head || !head->next)
-            return nullptr;
+        /***
+        *   We have two edges case:
+        *   1. !head: LL did not existed -> simply return null
+        *   2. !head->next: LL have only one node (head) 
+        *       and since n >= 1 (LL starting from 1) remove it would return null
+        */
+        if (!head || !head->next) return nullptr;
         
-        auto slow = head, fast = head, slow_prev = slow;
-        
+        auto slptr = head, fsptr = head, slprv = head;
+
         while (n > 0) {
-            fast = fast->next;
+            fsptr = fsptr->next;
             --n;
         }
         
-        if (!fast)
-            return head->next;
+        /***
+        *   if n = length of LL, fast would be at the end (...->next == <null> <- the end)
+        *   "to delete node n from behind" -> delete head of LL
+        *   -> just return next node from ptr;
+        */
+        if (!fsptr) return head->next;
         
-        while (fast) {
-            slow_prev = slow;
-            slow = slow->next;
-            fast = fast->next;
+        while (fsptr) {
+            slprv = slptr;
+            slptr = slptr->next;
+            fsptr = fsptr->next;
         }
         
-        slow_prev->next = slow->next;
-        slow = nullptr;
+        slprv->next = slptr->next;
+        slptr = nullptr;
         return head;
     }
 };
