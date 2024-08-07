59cb1508dbbf07de3e5cbc1155c6aba5a3056368
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-07 15:03:47 +0700

    Time: 4 ms (49.19%), Space: 11.9 MB (52.63%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,138 +1,67 @@
 class Solution {
-public:
-    string result(vector<string> &vstr) {
+    vector<string> mag00 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> mag10 = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> mag11 = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> mag20 = {"", "Hundred"};
+    vector<string> mag3 = {"", "Thousand", "Million", "Billion"};
+
+    string printresult(vector<string> &vstr) {
         string res;
-        for (int i = vstr.size() - 1; i >= 0; --i) {
-            res += vstr[i];
-            if (i > 0) res += " ";
+        for (const auto &s : vstr) {
+            if (!s.empty()) {
+                if (!res.empty()) res += " ";
+                res += s;
+            }
         }
         return res;
     }
     
-    void solve(int num, int mag, vector<string> &res) {
+    void solve(int num, vector<string> &vstr, int mag = 0) {
         if (num == 0) {
-            //Edge case: 0
-            res.push_back("Zero"); return;
+            vstr.push_back("Zero");
+            return;
         }
         
+        if (num == 0) return;
+        
         while (num > 0) {
-            if (num % 1000 == 0) {
-                // Check if this 10^3 block of num is just "000"
-                mag += 3;
-                num /= 1000;
-            } else {
-                //hangle mag-10 scale
-                switch (mag) {
-                    case (3):
-                        res.push_back("Thousand");
-                        break;
-                    case (6):
-                        res.push_back("Million");
-                        break;
-                    case (9):
-                        res.push_back("Billion");
-                        break;
-                    case (12):
-                        res.push_back("Thousand Billion");
-                        break;
-                    default:
-                        //num < 999
-                        break;
-                } 
-                int tmp;
-                /**
-                *   special case: when last digits is "10-19"
-                */
-                if (9 < num % 100 && num % 100 < 20) {
-                    tmp = num % 100;
-                    switch (tmp) {
-                        case (11): res.push_back("Eleven"); break;
-                        case (12): res.push_back("Twelve"); break;    
-                        case (13): res.push_back("Thirteen"); break;
-                        case (14): res.push_back("Fourteen"); break;
-                        case (15): res.push_back("Fifteen"); break;
-                        case (16): res.push_back("Sixteen"); break;
-                        case (17): res.push_back("Seventeen"); break;
-                        case (18): res.push_back("Eighteen"); break;
-                        case (19): res.push_back("Nineteen"); break;
-                        default:
-                            res.push_back("Ten");
-                            break;
-                    }
-                    mag += 2;
-                    num /= 100;
-                } else {
-                    // handle mag-1, from 0-9
-                    tmp = num % 10;
-                    switch(tmp) {
-                        case (1): res.push_back("One"); break;
-                        case (2): res.push_back("Two"); break;
-                        case (3): res.push_back("Three"); break;
-                        case (4): res.push_back("Four"); break;
-                        case (5): res.push_back("Five"); break;
-                        case (6): res.push_back("Six"); break;
-                        case (7): res.push_back("Seven"); break;
-                        case (8): res.push_back("Eight"); break;
-                        case (9): res.push_back("Nine"); break;
-                        default:
-                            break;
-                    };
-                    mag += 1;
-                    num /= 10;
-
-                    // handle mag-2, from 20-90
-                    tmp = num % 10;
-                    switch(tmp) {
-                        // case (1): s += "one"; break;
-                        case (1): res.push_back("Ten"); break;
-                        case (2): res.push_back("Twenty"); break;
-                        case (3): res.push_back("Thirty"); break;
-                        case (4): res.push_back("Forty"); break;
-                        case (5): res.push_back("Fifty"); break;
-                        case (6): res.push_back("Sixty"); break;
-                        case (7): res.push_back("Seventy"); break;
-                        case (8): res.push_back("Eighty"); break;
-                        case (9): res.push_back("Ninety"); break;
-                        default:
-                            break;
-                    };
-                    mag += 1;
-                    num /= 10;
+            int tmp = num % 1000;
+            num /= 1000;
+            
+            if (tmp > 0) {
+                vector<string> parts;
+                if (tmp >= 100) {
+                    parts.push_back(mag00[tmp / 100]);
+                    parts.push_back(mag20[1]); // "Hundred"
+                    tmp %= 100;
                 }
-
-                // hangle mag-3, from 100-900
-                tmp = num % 10;
-                switch(tmp) {
-                    case (1): res.push_back("One Hundred"); break;
-                    case (2): res.push_back("Two Hundred"); break;
-                    case (3): res.push_back("Three Hundred"); break;
-                    case (4): res.push_back("Four Hundred"); break;
-                    case (5): res.push_back("Five Hundred"); break;
-                    case (6): res.push_back("Six Hundred"); break;
-                    case (7): res.push_back("Seven Hundred"); break;
-                    case (8): res.push_back("Eight Hundred"); break;
-                    case (9): res.push_back("Nine Hundred"); break;
-                    default:
-                        break;
-                };
-                mag += 1;
-                num /= 10;    
+                
+                if (tmp >= 10 && tmp < 20) {
+                    parts.push_back(mag11[tmp - 10]);
+                } else {
+                    if (tmp >= 20) {
+                        parts.push_back(mag10[tmp / 10]);
+                        tmp %= 10;
+                    }
+                    if (tmp > 0) {
+                        parts.push_back(mag00[tmp]);
+                    }
+                }
+                
+                if (mag > 0) {
+                    parts.push_back(mag3[mag]);
+                }
+                
+                vstr.insert(vstr.begin(), parts.begin(), parts.end());
             }
+            mag++;
         }
     }
     
+public:
     string numberToWords(int num) {
-        /***
-        *   Ideas:
-        *   -> every 1000 increase the "word" from "" to maximum "thousand billion"
-        *   -> magnitude "" 1e0-3 (0-999) -> "thounsand" 1e4-6 (1,000-999,999) -> "million" 1e7-9 (1,000,000-999,999,999)
-        *   -> "billion" 1e10-12 (1,000,000,000-999,999,999,999) 
-        *   -> "thounsand billion" 1e13 (1,000,000,000,000-2,147,483,647)
-        *   We only need to calculate the calling for every 3-digits block and tack on the magnitude calling
-        */
-        
         vector<string> vstr;
-        solve(num, 0, vstr);
-        return result(vstr);
+        solve(num, vstr);
+        return printresult(vstr);
     }
 };
