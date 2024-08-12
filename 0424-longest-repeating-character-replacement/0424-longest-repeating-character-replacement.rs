120836971e7fa484055541208025c4602ab8e946
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-13 01:23:41 +0700

    Time: 1 ms (96.15%), Space: 2.1 MB (58.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
@@ -2,22 +2,21 @@ use std::cmp::max;
 
 impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
-        let charr:Vec<char> = s.chars().collect();
-        let (mut l, mut w_l, mut w_c) = (0, 0, 0);
-        let mut veca = vec![0;26];
+        let (s, k, A) = (s.as_bytes(), k as usize, 'A' as u8);
+        let (mut l, mut w_l, mut w_c, mut veca) = (0, 0, 0, vec![0;26]);
         
         for r in 0..s.len() {
-            let in_r = (charr[r] as u8 - 'A' as u8) as usize;
-            let in_l = (charr[l] as u8 - 'A' as u8) as usize;
+            let in_r = (s[r] - A) as usize;
+            let in_l = (s[l] - A) as usize;
             veca[in_r] += 1;
             w_c = max(w_c, veca[in_r]);
-            if r - l + 1 - w_c > k as usize {
+            if r - l + 1 - w_c > k {
                 veca[in_l] -= 1;
                 l += 1;
             }
             w_l = max(w_l, r - l + 1);
         }
         
-        return w_l as i32;
+        w_l as i32
     }
 }
