01cccb972864e297c778031166a6e6c116981f47
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-12 17:16:12 +0700

    Time: 27 ms (51.98%), Space: 24.8 MB (39.10%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,39 +1,26 @@
 class KthLargest {
-public:
-    vector<int> stream;
+private:
+    priority_queue<int, vector<int>, greater<int>> minHeap;
     int k;
 
+public:
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
+
         for (int num : nums) {
-            stream.push_back(num);
+            add(num);
         }
-        sort(stream.begin(), stream.end());
     }
 
     int add(int val) {
-        int index = getIndex(val);
-        // Add val to correct position
-        stream.insert(stream.begin() + index, val);
-        return stream[stream.size() - k];
-    }
-
-private:
-    int getIndex(int val) {
-        int left = 0;
-        int right = stream.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int midValue = stream[mid];
-            if (midValue == val) return mid;
-            if (midValue > val) {
-                // Go to left half
-                right = mid - 1;
-            } else {
-                // Go to right half
-                left = mid + 1;
+        // Add to our minHeap if we haven't processed k elements yet
+        // or if val is greater than the top element (the k-th largest)
+        if (minHeap.size() < k || minHeap.top() < val) {
+            minHeap.push(val);
+            if (minHeap.size() > k) {
+                minHeap.pop();
             }
         }
-        return left;
+        return minHeap.top();
     }
 };
