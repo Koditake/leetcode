260ecfef701dc53402611001cd5048082457a6c1
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-13 01:17:17 +0700

    Time: 1 ms (96.15%), Space: 2.3 MB (5.13%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
@@ -1,21 +1,23 @@
-use std::collections::HashMap;
 use std::cmp::max;
+
 impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
         let charr:Vec<char> = s.chars().collect();
         let (mut l, mut w_l, mut w_c) = (0, 0, 0);
-        let mut cmap = HashMap::new();
+        let mut veca = vec![0;26];
         
         for r in 0..s.len() {
-            *cmap.entry(charr[r]).or_insert(0) += 1;
-            w_c = max(w_c,*cmap.get(&charr[r]).unwrap());
-            if (r - l + 1) - w_c > k as usize {
-                cmap.entry(charr[l]).and_modify(|cnt| *cnt -= 1);
+            let in_r = (charr[r] as u8 - 'A' as u8) as usize;
+            let in_l = (charr[l] as u8 - 'A' as u8) as usize;
+            veca[in_r] += 1;
+            w_c = max(w_c, veca[in_r]);
+            if r - l + 1 - w_c > k as usize {
+                veca[in_l] -= 1;
                 l += 1;
             }
-            
             w_l = max(w_l, r - l + 1);
         }
+        
         return w_l as i32;
     }
 }
