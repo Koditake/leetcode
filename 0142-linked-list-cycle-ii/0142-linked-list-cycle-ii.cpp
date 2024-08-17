08af5d0706d99b6a548dbbe5cbb78c8f66b3f61a
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-17 18:49:35 +0700

    Time: 29 ms (5.25%), Space: 10.2 MB (60.47%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -21,18 +21,37 @@ public:
     }
     
     ListNode *detectCycle(ListNode *head) {
+        /** Idea:
+        * Let   L1 = start of cycle in LL
+        *       l1 = dist head -> L1
+        *       L2 = meeting of slow&fast ptrs in Floyd's algorithm
+        *       l2 = dist L1 -> L2
+        *       XP = the dist from L2 to L1 in cycle
+        * When slow meets fast,
+        *       dist of slow: l1 + l2
+        *       dist of fast: l1 + l2 + XP + l2;
+        * Since 2Vs = Vf, w.r.t same travel time:
+        *       ==> 2(l1 + l2) = l1 + l2 + XP + l2;
+        *       <=> 2*l1 + 2*l2 = l1 + 2*l2 +  XP
+        *       <=> l1 = XP;
+        * This mean H -> L1 = L2 -> L1;
+        * Plan:
+        * Find if LL is cycle. If yes, mark the meeting ptr of Floyd's
+        * Make new cur ptr from H. Run cur and slow and same speed.
+        * If cur meets slow -> that's where cycle head is.
+        */
+        
         auto cyl = isCycle(head);
         if (!head || !cyl) return nullptr;
-        /** Idea:
-        *   Run a third ptr fron head
-        *   For every node of third ptr, run the cycle at normal speed.
-        *   If a full cycle does not meet the third ptr, move to the next node.
-        *   Repeat ad naseaum
-        */
+        
         auto cur = head;
-        while (cur != cyl) {
+        while (cur) {
+            auto pre = cyl;
+            do {
+                cyl = cyl->next;
+                if (cur == cyl) return cur;
+            } while (pre != cyl);
             cur = cur->next;
-            cyl = cyl->next;
         }
         
         return cur;
