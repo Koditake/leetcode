78b17d989bc2771447b3d8390e7966a2c813bd41
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-17 17:56:42 +0700

    Time: 26 ms (5.25%), Space: 10.4 MB (52.62%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    auto isCycle(ListNode *head) -> ListNode* {
+        auto slow = head, fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return slow;
+        }
+        
+        return nullptr;
+    }
+    
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !isCycle(head)) return nullptr;
+        /** Idea:
+        *   Run a third ptr fron head
+        *   For every node of third ptr, run the cycle at normal speed.
+        *   If a full cycle does not meet the third ptr, move to the next node.
+        *   Repeat ad naseaum
+        */
+        auto cur = head, cyl = isCycle(head);
+        while (cur) {
+            auto pre = cyl;
+            do {
+                cyl = cyl->next;
+                if (cur == cyl) return cur;
+            } while (pre != cyl);
+            cur = cur->next;
+        }
+        
+        return cur;
+    }
+};
