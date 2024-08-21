33bf6f4a6ed1d1c06f879de5d472c95f99ad3781
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-21 15:08:52 +0700

    Time: 122 ms (15.19%), Space: 71.9 MB (7.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool checkInclusion(string &s1, string &s2) {
-        if (s1.length() > s2.length()) return false;
+        int n1 = s1.size(),
+            n2 = s2.size();
         
-        unordered_map<char, int> map1;
-        for (auto const &c:s1) ++map1[c];
+        if (n1 > n2) return false;
         
-        for (int i = 0; i <= s2.length() - s1.length(); ++i) {
-            unordered_map<char, int> map2;
-            auto s2b = s2.substr(i,s1.length());
-            for (auto const &c:s2b) ++map2[c];
+        vector<int> map1 (26,0);
+        for (auto const &c:s1) ++map1[c - 'a'];
+        for (int i = 0; i <= n2 - n1; ++i) {
+            vector<int> map2 (26,0); 
+            auto s2b = s2.substr(i, n1);
+            for (auto const &c:s2b) ++map2[c - 'a'];
             if (map1 == map2) return true;
         }
         
