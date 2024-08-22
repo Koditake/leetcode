5d2b08cd8f7b4d733bf07fb6d2f43c76bc845e3e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-22 22:35:30 +0700

    Time: 8 ms (88.18%), Space: 17.5 MB (45.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> topKFrequent(vector<int>& nums, int k) {  
+        if (nums.size() == k) return nums;
         
         unordered_map<int,int> nmap;
         for (auto const &n:nums) ++nmap[n];
-        int n = nmap.size();
-        priority_queue<pair<int,int>> pq;
-        vector<int> res;
+        
+        priority_queue<pair<int,int>> pq; vector<int> res;
         for (auto const &[num,freq]:nmap) {
             pq.push({freq,num});
-            if (pq.size() >= n - k + 1) {
+            if (pq.size() > nmap.size() - k) {
                 res.push_back(pq.top().second);
                 pq.pop();
             }
