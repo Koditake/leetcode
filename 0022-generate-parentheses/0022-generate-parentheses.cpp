c0c9bfdc87b784a64ec4daf3355637ba8cb83d40
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-25 17:33:01 +0700

    Time: 3 ms (60.50%), Space: 20.3 MB (8.42%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    auto helper(std::string str, int o, int c, vector<string> &res) -> void {
+        if (!o && !c) {
+            res.push_back(str);
+            return;
+        }
+        
+        if (o < c) helper(str + ')', o, c - 1, res);
+        if (o >= 0) helper(str + '(', o - 1, c, res);
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        helper("", n, n, res);
+        return res;
+    }
+};
