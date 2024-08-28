b74c81e1749f0ecd188d285205d63201490e4c51
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-08-28 15:58:27 +0700

    Time: 7 ms (64.55%), Space: 8.6 MB (90.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,23 +6,31 @@ public:
         
         if (n1 > n2) return false;
         
-        vector<int> vec1 (26,0), 
-                    vec2 (26,0);
-        
+        vector<int> m1 (26,0), m2 (26,0);
+        int cnt = 0;
         for (int i = 0; i < n1; ++i) {
-            ++vec1[s1[i] - 'a'];
-            ++vec2[s2[i] - 'a'];
+            ++m1[s1[i] - 'a'];
+            ++m2[s2[i] - 'a'];
         }
         
-        if (vec1 == vec2) return true;
+        for (int i = 0; i < 26; ++i) if (m1[i] == m2[i]) ++cnt;
         
-        int r = 0;
         
-        for (r = n1; r < n2; ++r) {
-            --vec2[s2[r - n1] - 'a'];
-            ++vec2[s2[r] - 'a'];
-            if (vec2 == vec1) return true;
+        for (int i = n1; i < n2; ++i) {
+            if (cnt == 26) return true;
+            
+            int r = s2[i] - 'a',
+                l = s2[i - n1] - 'a';    
+            
+            ++m2[r];
+            if (m2[r] == m1[r]) ++cnt; 
+            else if (m2[r] == m1[r] + 1) --cnt;
+            
+            --m2[l];
+            if (m2[l] == m1[l]) ++cnt;
+            else if (m2[l] == m1[l] - 1) --cnt;
         }
-        return false;
+        
+        return cnt == 26;
     }
 };
