13fe0df6ef63cd5c6d6976e1c4686719ed18d020
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-09-02 08:25:55 +0700

    Time: 97 ms (52.44%), Space: 77.1 MB (18.08%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int n= chalk.size();
-        long long arr[n];
-        arr[0]= chalk[0];
-        for (int i=1; i<n; i++)arr[i]=arr[i-1]+ chalk[i];
-        k = k%arr[n-1];
-        // just greater than k;
-        int low=0; int high= n-1;
-        //int ans=0;
-        while (low<=high){
-            int mid= low + (high-low)/2;
-            if (arr[mid]> k){
-                //ans= mid;
-                high= mid-1;
-            }
-            else{
-                low= mid+1;
+        // Find the sum of all elements.
+        int sum = 0;
+        for (int i = 0; i < chalk.size(); i++) {
+            sum += chalk[i];
+            if (sum > k) {
+                break;
             }
         }
-        return low;
+        // Find modulo of k with sum.
+        k = k % sum;
+        for (int i = 0; i < chalk.size(); i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k = k - chalk[i];
+        }
+        return 0;
     }
 };
