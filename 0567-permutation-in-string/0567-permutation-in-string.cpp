f61fe7b43b852c771ffa6a515927bdd3a6cd0d3e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-09-05 00:36:51 +0700

    Time: 821 ms (10.50%), Space: 135.4 MB (5.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    bool checkInclusion(string &s1, string &s2) {
-        int n1 = s1.size(),
+    bool checkInclusion(string s1, string s2) {
+        int l = 0, 
+            r = 0, 
+            n1 = s1.size(), 
             n2 = s2.size();
         
         if (n1 > n2) return false;
         
-        vector<int> m1 (26,0), m2 (26,0);
-        int cnt = 0;
-        for (int i = 0; i < n1; ++i) {
-            ++m1[s1[i] - 'a'];
-            ++m2[s2[i] - 'a'];
+        unordered_map<char,int> m1;
+        
+        for (auto const &c:s1) ++m1[c];
+        
+        for (int i = 0; i <= n2 - n1; ++i) {
+            unordered_map<char,int> m2;
+            auto str = s2.substr(i, n1);
+            for (auto const &c:str) ++m2[c];
+            if (m2 == m1) 
+                return true;
         }
         
-        for (int i = 0; i < 26; ++i) if (m1[i] == m2[i]) ++cnt;
-        
-        
-        for (int i = n1; i < n2; ++i) {
-            if (cnt == 26) return true;
-            
-            int r = s2[i] - 'a',
-                l = s2[i - n1] - 'a';    
-            
-            ++m2[r];
-            if (m2[r] == m1[r]) ++cnt; 
-            else if (m2[r] == m1[r] + 1) --cnt;
-            
-            --m2[l];
-            if (m2[l] == m1[l]) ++cnt;
-            else if (m2[l] == m1[l] - 1) --cnt;
-        }
-        
-        return cnt == 26;
+        return false;
     }
 };
