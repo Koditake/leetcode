dc32951b1b0356525a95c1e1762e1b9ca0a020f8
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-09-05 01:12:10 +0700

    Time: 421 ms (6.36%), Space: 2.3 MB (10.00%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.rs
+++ 0567-permutation-in-string/0567-permutation-in-string.rs
@@ -4,17 +4,20 @@ impl Solution {
     pub fn check_inclusion(s1: String, s2: String) -> bool {
         let (n1,n2) = (s1.len(), s2.len());
         if n1 > n2 { return false; }
-        let (charr1, charr2):(Vec<char>, Vec<char>) = (s1.chars().collect(), s2.chars().collect());
+        
+        let charr1:Vec<char> = s1.chars().collect();
         let mut m1 = HashMap::new();
+        
         for c in charr1 {
             *m1.entry(c).or_insert(0) += 1;
         }
         
         for i in n1..n2 + 1 {
-            let charr3 = &charr2[i - n1..i];
+            let s3 = &s2[i - n1..i];
+            let charr3:Vec<char> = s3.chars().collect();
             let mut m2 = HashMap::new();
             for c in charr3 {
-                *m2.entry(*c).or_insert(0) += 1;
+                *m2.entry(c).or_insert(0) += 1;
             }
             
             if m1 == m2 { return true; }
