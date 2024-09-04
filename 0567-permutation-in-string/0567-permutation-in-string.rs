63cfa439fffc17d838124ca6f0ea01cb0d9ce2e7
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-09-05 01:16:39 +0700

    Time: 0 ms (100.00%), Space: 2.1 MB (61.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.rs
+++ 0567-permutation-in-string/0567-permutation-in-string.rs
@@ -1,25 +1,21 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn check_inclusion(s1: String, s2: String) -> bool {
         let (n1,n2) = (s1.len(), s2.len());
         if n1 > n2 { return false; }
         
-        let charr1:Vec<char> = s1.chars().collect();
-        let mut m1 = HashMap::new();
+        let (charr1,charr2):(Vec<char>,Vec<char>) = (s1.chars().collect(),s2.chars().collect());
+        let (mut m1, mut m2) = (vec![0;26], vec![0;26]);
         
-        for c in charr1 {
-            *m1.entry(c).or_insert(0) += 1;
+        for i in 0..n1 {
+            m1[(charr1[i] as u8 - 'a' as u8) as usize] += 1;
+            m2[(charr2[i] as u8 - 'a' as u8) as usize] += 1;
         }
         
-        for i in n1..n2 + 1 {
-            let s3 = &s2[i - n1..i];
-            let charr3:Vec<char> = s3.chars().collect();
-            let mut m2 = HashMap::new();
-            for c in charr3 {
-                *m2.entry(c).or_insert(0) += 1;
-            }
-            
+        if m1 == m2 { return true; }
+        
+        for i in n1..n2 {
+            m2[(charr2[i] as u8 - 'a' as u8) as usize] += 1;
+            m2[(charr2[i - n1] as u8 - 'a' as u8) as usize] -= 1;
             if m1 == m2 { return true; }
         }
         
