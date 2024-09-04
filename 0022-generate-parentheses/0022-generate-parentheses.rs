42f5beb469159f702b50e1c78008c0c7bc2e2c96
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-09-05 02:12:13 +0700

    Time: 1 ms (84.27%), Space: 2.3 MB (12.50%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.rs
+++ 0022-generate-parentheses/0022-generate-parentheses.rs
@@ -1,22 +1,22 @@
 impl Solution {
-    fn helper(o: i32, c: i32, str: String, res: &mut Vec<String>) {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut vecstr:Vec<String> = Vec::new();
+        Self::helper(n,n,String::new(),&mut vecstr);
+        return vecstr;
+    }
+    
+    fn helper(o:i32, c:i32, str:String, vecstr: &mut Vec<String>) {
         if o == 0 && c == 0 {
-            res.push(str);
+            vecstr.push(str);
             return;
         }
         
-        if c > o {
-            Solution::helper(o, c - 1, str.clone() + ")", res);
+        if (o < c) {
+            Self::helper(o, c - 1, str.clone() + ")", vecstr);
         }
         
-        if o > 0 {
-            Solution::helper(o - 1, c, str + "(", res);
+        if (o > 0) {
+            Self::helper(o - 1, c, str.clone() + "(", vecstr);
         }
-    }
-    
-    pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        let mut res:Vec<String> = Vec::new();
-        Solution::helper(n, n, String::new(), &mut res);
-        res
     }
 }
