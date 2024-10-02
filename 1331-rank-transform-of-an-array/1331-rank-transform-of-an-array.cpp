8e09984a5a5a984138d2a49636af3b0da1a964bf
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-03 06:44:42 +0700

    Time: 94 ms (36.89%), Space: 51.7 MB (5.71%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        // Store the rank for each number in arr
-        unordered_map<int, int> numToRank;
-        // Deduplicate and sort arr
-        set<int> nums(arr.begin(), arr.end());
-        int rank = 1;
-        for (auto num : nums) {
-            numToRank[num] = rank;
-            rank++;
-        }
+        // Store the rank for each number in arr.
+        map<int, vector<int>> numToIndices;
+
         for (int i = 0; i < arr.size(); i++) {
-            arr[i] = numToRank[arr[i]];
+            numToIndices[arr[i]].push_back(i);
+        }
+
+        int rank = 1;
+        for (auto& pair : numToIndices) {
+            for (int index : pair.second) {
+                arr[index] = rank;
+            }
+            rank++;
         }
         return arr;
     }
