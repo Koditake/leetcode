c903b6b7b92a1145961b4b2ccf3e408c84bdfffa
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-06 03:37:16 +0700

    Time: 778 ms (10.52%), Space: 135.4 MB (5.00%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,20 +4,13 @@ public:
         int n1 = s1.size(),
             n2 = s2.size();
         
-        if (n1 > n2) return false;
+        unordered_map<char,int> m1;
+        for (auto const &c:s1) ++m1[c];
         
-        vector<int> m1 (26,0), m2 (26,0);
-        
-        for (int i = 0; i < n1; ++i) {
-            ++m1[s1[i] - 'a'];
-            ++m2[s2[i] - 'a'];
-        }
-        
-        if (m1 == m2) return true;
-        
-        for (int i = n1; i < n2; ++i) {
-            ++m2[s2[i] - 'a'];
-            --m2[s2[i - n1] - 'a'];
+        for (int i = 0; i <= n2 - n1; ++i) {
+            auto str = s2.substr(i,n1);
+            unordered_map<char,int> m2;
+            for (auto const &c:str) ++m2[c];
             if (m1 == m2) return true;
         }
         
