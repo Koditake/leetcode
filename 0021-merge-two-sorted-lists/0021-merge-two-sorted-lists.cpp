b3fe9ee9b02942434bc1628990348e070f300e6d
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-06 14:25:10 +0700

    Time: 3 ms (86.05%), Space: 19.8 MB (56.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,18 +11,15 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1 && !list2) return nullptr;
+        if (list1 == nullptr) return list2;
+        else if( list2 == nullptr ) return list1;
         
-        if (!list1 ^ !list2) return !list1?list2:list1;
-        
-        if (list1->val < list2->val) {
+        if (list1->val < list2->val){
             list1->next = mergeTwoLists(list1->next,list2);
             return list1;
         } else {
-            list2->next = mergeTwoLists(list1,list2->next); 
+            list2->next = mergeTwoLists(list1,list2->next);
             return list2;
         }
-        
-        return nullptr;
     }
 };
