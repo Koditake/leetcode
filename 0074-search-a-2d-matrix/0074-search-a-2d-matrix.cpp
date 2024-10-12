93214c9c5aa9a50a50c0e077305ad784e73990e4
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-12 16:46:29 +0700

    Time: 7 ms (21.57%), Space: 12.1 MB (92.50%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size(),
-            col = matrix[0].size();
+        int r = matrix.size(),
+            c = matrix[0].size();
         
-        // check if target overshoot matrix
-        if (target < matrix[0][0] || matrix[row - 1][col - 1] < target)
-            return false;
-
-        int l = 0,
-            r = row * col - 1;
+        if (target < matrix[0][0] || target > matrix[r - 1][c - 1]) return false;
+        if (target == matrix[0][0] || target == matrix[r - 1][c - 1]) return true;
+        int s = 0,
+            e = r * c - 1;
         
-        while (l <= r) {
-            int m = l + (r - l)/2,
-                v = matrix[m / col][m % col];
-            if (target < v) {
-                r = m - 1;
-            } else if (target > v) {
-                l = m + 1;
-            } else /* target == v */ { 
+        while (s <= e) {
+            int m = s + (e - s)/2,
+                mid = matrix[m / c][m % c];
+            if (mid < target) {
+                s = m + 1;
+            } else if (mid > target) {
+                e = m - 1;
+            } else {
                 return true;
             }
         }
