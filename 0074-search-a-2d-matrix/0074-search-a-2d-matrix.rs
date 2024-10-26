25b4d8eec7c48b09f1bd3bbe40132f6b5f08e7fc
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-26 09:52:38 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (11.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -1,30 +1,25 @@
-use std::cmp::Ordering;
-
+use std::cmp::Ordering::{Less,Greater,Equal};
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        let (row, col) = (matrix.len(), matrix[0].len());
+        let m = matrix.len();
+        let n = matrix[0].len();
         
-        // check if target overshoot matrix
-        if target < matrix[0][0] || matrix[row - 1][col - 1] < target {
+        if target < matrix[0][0] || matrix[m - 1][n - 1] < target {
+            // target is overmatched by matrix
             return false;
         }
         
-        //check if target is at begin/end matrix
-        if target == matrix[0][0] || matrix[row - 1][col - 1] == target {
-            return true;
-        }
+        let (mut i, mut j) = (0, m * n - 1);
         
-        let mut l = 0;
-        let mut r = row * col - 1;
-        
-        while l < r {
-            let m = l + (r - l)/2;
-            let v = matrix[m / col][m % col];
-            
-            match target.cmp(&v) {
-                Ordering::Less => r = m,
-                Ordering::Greater => l = m + 1,
-                Ordering::Equal => return true,
+        while i <= j {
+            let x = (i + (j - i)/2) as usize;
+            let v = matrix[x/n][x%n];
+            if target == v {
+                return true;
+            } else if target > v {
+                i = x + 1;
+            } else {
+                j = x - 1;
             }
         }
         
