d8acd5e66d023f65163813560e6ac4e09ee5deba
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-10-26 10:37:06 +0700

    Time: 0 ms (100.00%), Space: 15.6 MB (82.76%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> stn;
-        for(auto s:tokens) {
-            if(s.size()> 1 || isdigit(s[0])) 
-                //either s is a single digit or a multiple digits number
-                stn.push(stoi(s));
-            else {
-                auto x2=stn.top(); 
-                    stn.pop();
-                auto x1=stn.top(); 
-                    stn.pop();
-                switch(s[0]) {
-                    case '+': x1+=x2; break;
-                    case '-': x1-=x2; break;
-                    case '*': x1*=x2; break;
-                    case '/': x1/=x2; break;
-                }
-                stn.push(x1);
-            }
+        vector<int> temp;
+        for (auto const t:tokens) {
+            if (!temp.empty() && t == "+" || t == "-" || t == "*" || t == "/") {
+                int second = temp.back();
+                temp.pop_back();
+                int first = temp.back();
+                temp.pop_back();
+                int res = 0;
+                
+                if (t == "+") res = first + second;
+                if (t == "-") res = first - second;
+                if (t == "*") res = first * second;
+                if (t == "/") res = first / second;
+                
+                temp.push_back(res);
+            } else temp.push_back(stoi(t));
         }
-        return stn.top();
+        
+        return temp.back();
     }
 };
