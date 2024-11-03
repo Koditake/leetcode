619a8ed22d2b7f2dc40c23fab7c8e284ac1de23e
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-11-03 20:47:42 +0700

    Time: 0 ms (100.00%), Space: 7.5 MB (83.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
-        
-        
-        for (int i = 0; i < s.size(); ++i) {
-            if (s == goal) return true;
-            for (int i = 0; i < s.size() - 1; ++i)
-                std::swap(s[i], s[i + 1]);
-        }
-        
-        return false;
+
+        // Create a new string by concatenating 's' with itself
+        string doubledString = s + s;
+
+        return doubledString.find(goal) < doubledString.length();
     }
 };
