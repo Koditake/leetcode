f3ab1c82aa334d055b968b7d63e044f754ee8b43
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-12-13 15:17:20 +0700

    Time: 5 ms (58.65%), Space: 11.1 MB (11.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int> mp;
-        int l = 0, r = 0, res = 0, cmax = 0;
-        for (r; r < s.length(); ++r) {
-            ++mp[s[r] - 'A'];
-            cmax = max(mp[s[r] - 'A'], cmax);
-            while (r - l + 1 - cmax > k) {
-                --mp[s[l] - 'A'];
+        unordered_map<char,int> cmap;
+        int l = 0, r = 0, n = s.length(), res = 0, c = 0;
+        for (r; r < n; ++r) {
+            ++cmap[s[r] - 'A'];
+            c = max(c,cmap[s[r] - 'A']);
+            while (r - l + 1 - c > k) {
+                --cmap[s[l] - 'A'];
                 ++l;
             }
             res = max(res, r - l + 1);
         }
-        
         return res;
     }
 };
