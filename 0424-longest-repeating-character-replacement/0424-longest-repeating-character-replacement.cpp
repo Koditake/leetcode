937feddad6760f3e15252387f04741edb754fd82
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-12-16 18:37:30 +0700

    Time: 4 ms (64.52%), Space: 11 MB (18.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int> cmap;
+        //use a int array
+        std::array<int,26> cmap;
         
         int l = 0, r = 0, m = 0, res = 0;
         
         for (r; r < s.size(); ++r) {
-            ++cmap[s[r]];
+            ++cmap[s[r] - 'A'];
             //Find if current character is the longest character in our window
-            m = max(m, cmap[s[r]]);
+            m = max(m, cmap[s[r] - 'A']);
             // Find if replace the longest character in our window m
             // still within our budget of k chacters
             while (r - l + 1 - m > k) {
-                --cmap[s[l]];
+                --cmap[s[l] - 'A'];
                 ++l;
             }
             res = max(res, r - l + 1);
