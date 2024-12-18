830b17621b1deae8c5907bf8fb193af01be6d4fa
Tuan Nguyen <thientuannguyen95@gmail.com>
2024-12-18 17:50:50 +0700

    Time: 0 ms (100.00%), Space: 7.9 MB (5.97%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        long x = 1;
-        
-        while (x * x < num) {
-            ++x;
+        int l = 0,  r = num;
+        while (l <= r) {
+            int m = l + (r - l)/2;
+            auto tmp = (long long)m*m;
+            if (tmp < num) l = m + 1;
+            else if (tmp > num) r = m - 1;
+            else return true;
         }
         
-        return x * x == num;
+        return false;
     }
 };
